849e5d1dbb8c81010c29e1e9c6f1afb4c992f2e5
Michael E. Cotterell <mepcotterell@gmail.com>
2025-08-08 22:39:53 -0400

    switched back to headless

--- cs1302_code_visualizer/browser_driver.py
+++ cs1302_code_visualizer/browser_driver.py
@@ -38,7 +38,7 @@ def get_webdriver(dpi: int = 1) -> webdriver.Chrome:
         The webdriver used to display the frontend.
     """
     options = Options()
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument(f"--force-device-scale-factor={dpi}")
     options.add_argument("--allow-file-access-from-files")
     options.add_argument("--no-sandbox")
