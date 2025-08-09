4010f02d3f3cca3bf15f08e9d2b770ba6313af77
Michael E. Cotterell <mepcotterell@gmail.com>
2025-08-09 00:34:00 -0400

    webdriver no longer detaches

--- cs1302_code_visualizer/browser_driver.py
+++ cs1302_code_visualizer/browser_driver.py
@@ -39,7 +39,7 @@ def get_webdriver(dpi: int = 1) -> webdriver.Chrome:
         The webdriver used to display the frontend.
     """
     options = Options()
-    options.add_experimental_option("detach", True)
+    # options.add_experimental_option("detach", True)
     options.add_argument("--headless=new")
     options.add_argument(f"--force-device-scale-factor={dpi}")
     options.add_argument("--allow-file-access-from-files")
